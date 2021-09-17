97c68777f962a3e284d15bc63f2901e2a1b4a4cc
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-17 20:45:42 +0300

    no single source of truth

--- composing_components/src/components/counters.jsx
+++ composing_components/src/components/counters.jsx
@@ -9,14 +9,27 @@ class Counters extends React.Component {
             { id: 3, value: 0 },
             { id: 4, value: 0 },
         ]
-    }
+    };
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
     handleDelete = (counterId) => {
         console.log("Event handler called", counterId);
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({ counters });
-    }
+    };
     render() {
         return <div>
+            <button
+                onCLick={this.handleReset}
+                className="btn btn-primary btn-sm m-2"
+            >
+                Reset
+            </button>
             {this.state.counters.map(counter =>
                 <Counter
                     key={counter.id}
