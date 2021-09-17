35d9e7399db84de6945c7fa63e504ab92ceb78d2
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-17 20:40:28 +0300

    functioning delete event

--- composing_components/src/components/counter.jsx
+++ composing_components/src/components/counter.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 class Counter extends React.Component {
     state = {
-        value: this.props.value
+        value: this.props.counter.value
     };
 
     handleIncrement = () => {
@@ -19,7 +19,7 @@ class Counter extends React.Component {
                 Increment
             </button>
             <button
-                onClick={this.props.onDelete}
+                onClick={() => this.props.onDelete(this.props.counter.id)}
                 className="btn btn-danger btn-sm m-2"
             >
                 Delete
--- composing_components/src/components/counters.jsx
+++ composing_components/src/components/counters.jsx
@@ -10,14 +10,19 @@ class Counters extends React.Component {
             { id: 4, value: 0 },
         ]
     }
-    handleDelete = () => {
-        console.log("Event handler called");
-
+    handleDelete = (counterId) => {
+        console.log("Event handler called", counterId);
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters });
     }
     render() {
         return <div>
             {this.state.counters.map(counter =>
-                <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id} />
+                <Counter
+                    key={counter.id}
+                    onDelete={this.handleDelete}
+                    counter={counter}
+                />
             )}
         </div>;
     }
