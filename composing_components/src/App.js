bdae21fc9eee4a0926cdb51b398ad94639f63819
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-17 21:26:58 +0300

    counters counter

--- composing_components/src/App.js
+++ composing_components/src/App.js
@@ -31,7 +31,7 @@ class App extends React.Component {
     };
     render() {
         return <React.Fragment>
-            <NavBar />;
+            <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />;
             <main className="container">
                 <Counters
                     counters={this.state.counters}
--- composing_components/src/components/navbar.jsx
+++ composing_components/src/components/navbar.jsx
@@ -4,6 +4,9 @@ class NavBar extends React.Component {
         return <nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">
                 Navbar
+                <span className="badge badge-pill badge-secondary">
+                    {this.props.totalCounters}
+                </span>
             </a>
         </nav>;
     }
