bbec432a7badfbb6ccf2789761cf38fea2ba5944
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-17 21:24:08 +0300

    making app root component

--- composing_components/src/App.js
+++ composing_components/src/App.js
@@ -3,11 +3,42 @@ import NavBar from './components/navbar';
 import Counters from "./components/counters";
 
 class App extends React.Component {
+    state = {
+        counters: [
+            { id: 1, value: 0 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+            { id: 4, value: 0 },
+        ]
+    };
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters });
+    };
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = { ...counter };
+        counters[index].value++;
+        this.setState({ counters });
+    };
+    handleDelete = (counterId) => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters });
+    };
     render() {
         return <React.Fragment>
             <NavBar />;
             <main className="container">
-                <Counters />
+                <Counters
+                    counters={this.state.counters}
+                    onReset={this.handleReset}
+                    onIncrement={this.handleIncrement}
+                    onDelete={this.handleDelete}
+                />
             </main>
         </React.Fragment>
     }
