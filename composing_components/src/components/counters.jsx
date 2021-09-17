630737a0f826767e61ecf08292f75ffa674a3074
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-17 21:05:53 +0300

    fixed typos

--- composing_components/src/components/counters.jsx
+++ composing_components/src/components/counters.jsx
@@ -16,17 +16,22 @@ class Counters extends React.Component {
             return c;
         });
         this.setState({ counters });
-        // local state isn't updated, thats why we need a single source of truth.
+    };
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = { ...counter };
+        counters[index].value++;
+        this.setState({ counters });
     };
     handleDelete = (counterId) => {
-        console.log("Event handler called", counterId);
         const counters = this.state.counters.filter(c => c.id !== counterId);
         this.setState({ counters });
     };
     render() {
         return <div>
             <button
-                onCLick={this.handleReset}
+                onClick={this.handleReset}
                 className="btn btn-primary btn-sm m-2"
             >
                 Reset
@@ -35,6 +40,7 @@ class Counters extends React.Component {
                 <Counter
                     key={counter.id}
                     onDelete={this.handleDelete}
+                    onIncrement={this.handleIncrement}
                     counter={counter}
                 />
             )}
