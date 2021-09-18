4b6983e3016cfd0db41a53ebb928506170662c12
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-18 13:14:37 +0300

    set initial state

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -17,7 +17,10 @@ const allReducers=combineReducers({
   user: userReducer
 })
 
-const store = createStore(allReducers);
+const store = createStore(allReducers,{
+  products: [{ name: 'iPhone' }],
+  user: 'Michael'
+});
 
 console.log(store.getState());
 
