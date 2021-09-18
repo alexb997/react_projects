d2d911f540efc2690c90d015588bc094d858c8a5
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-18 13:04:32 +0300

    store creation

--- redux-tutorial/src/index.js
+++ redux-tutorial/src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {createStore} from 'redux';
+
+function reducer(){
+  return 'State';
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
