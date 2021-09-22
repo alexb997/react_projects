e93e6f50182a127fde454d251592d6a658280ee6
alexb997 <alex_alexandru_997@yahoo.ro>
2021-09-22 19:39:20 +0300

    multiple components

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,3 +1,5 @@
+import CoursesList from "./CoursesList";
+
 const courses =[
   {
     id:1,
@@ -28,32 +30,14 @@ const courses =[
   }
 ]
 
+
+
 function App() {
-  
   return (
     <div>
       <h1>List of Courses</h1>
       <hr />
-      {courses.map(function(course) {
-        return (<div key= {course.id}>
-            <span>
-              <a href={course.url}><h4>{course.title}</h4></a>
-            </span>
-            <span>
-              by <strong>{course.author}</strong>
-            </span>
-            <span>
-              | Video Hours: {course.hours_video}
-            </span>
-            <span>
-              | Number of Lectures: {course.number_of_lectures}
-            </span>
-            <span>
-              | Rating: {course.rating}
-            </span>
-            <br/><br/>
-          </div>);
-      })}
+      <CoursesList courses={courses}/>
     </div>
   );
 }
